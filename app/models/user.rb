2f6414e4e7548d18c4b0e5ebb1e0d64aeeea06e0
vagrant <vagrant@vagrant.com>
2019-10-30 03:48:32 +0000

    Created User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :real_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitivce: true }, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, length: { maximum: 20 }
+  validates :gender, presence: true
+  validates :birthdate, length: { maximum: Time.now}
+  VALID_CODE_REGEX = /\A[0-9]+[0-9]+[0-9]+[-]+[0-9]+[0-9]+[0-9]+[0-9]\z/i
+  validates :code, presence: true, length: { is: 8 } format: { with: VALID_CODE_REGEX }
+  validates :address, presence: true, length: { maximum: 255 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029063859) do
+ActiveRecord::Schema.define(version: 20191030034543) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "category"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20191029063859) do
     t.datetime "reset_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
